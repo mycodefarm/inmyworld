1d8efbc355bf2cdc2fe809444a7c967cfaa4c63d
mycodefarm <15982406684@163.com>
2017-12-28 22:11:21 +0800

    add doit

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
 
   constructor(props) {
     super(props);
-    this.setState = {
+    this.state = {
 
     }
     this.showMessage = this.showMessage.bind(this);
--- src/doit/DoIt.js
+++ src/doit/DoIt.js
@@ -1,10 +1,76 @@
 import React, { Component } from 'react';
+import { Input } from 'antd';
+import './Doit.css';
+
+class TimeRecord extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            elapsed: 0
+        }
+        this.tick = this.tick.bind(this);
+    }
+
+    componentDidMount() {
+        this.timerID = setInterval(
+            () => this.tick(),
+            100
+        );
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+
+    tick() {
+        this.setState({
+            elapsed: new Date() - this.props.start
+        });
+    }
+
+    render() {
+        var elapsed = Math.round(this.state.elapsed / 100);
+        var seconds = (elapsed / 10).toFixed(1);
+
+        return (
+            <div>
+                <p>时间已过{seconds}秒</p>
+            </div>
+        );
+    }
+}
 
 class Doit extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            searchText: ""
+        }
+        this.onChange = this.onChange.bind(this);
+        this.onBegin = this.onBegin.bind(this);
+    }
+
+    onChange(event) {
+        this.setState({
+            searchText: event.target.value
+        });
+    }
+
+    //开启计时
+    onBegin() {
+        console.log(this.state.searchText)
+    }
+
     render() {
         return (
-            <div>hello doit</div>
+            <div className="main">
+                <Input size="large" placeholder="说一些你想说的话,做一些你想做的事"
+                    style={{ width: 600 }} onChange={this.onChange} onPressEnter={this.onBegin} />
+                <TimeRecord start={Date.now()} />
+                <p>{this.state.searchText}</p>
+            </div>
         )
     }
 }
